513192aeb7c335c9d346080db912d601b45d57e9
PROKILLER24 <suyashwagade@gmail.com>
2025-09-25 01:07:51 +0530

    Png and thing's are added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/boy image.png")}
+        src={getImageUrl("hero/Suyash.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
